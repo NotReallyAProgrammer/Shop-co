52c79f120aedc1d3493373634c9294f9b26559c6
NotReallyAProgrammer <73622276+NotReallyAProgrammer@users.noreply.github.com>
2023-12-09 20:54:46 +0800

    Header Mobile

--- src/app/layouts/header/header.component.ts
+++ src/app/layouts/header/header.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styleUrls: ['./header.component.css'],
 })
 export class HeaderComponent {
+  isNavOpen: boolean = false;
+  offsetFlag: boolean = true;
 
+  openNav(): void {
+    this.isNavOpen = !this.isNavOpen;
+  }
+
+  @HostListener('window:scroll', ['$event']) getScrollHeight(event: any) {
+    if (window.scrollY > 0) this.isNavOpen = !this.isNavOpen;
+  }
 }
