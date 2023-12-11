d8bbb0c2040501e104e906369147192e4b60221d
NotReallyAProgrammer <73622276+NotReallyAProgrammer@users.noreply.github.com>
2023-12-11 20:25:29 +0800

    Footer

--- src/app/layouts/header/header.component.ts
+++ src/app/layouts/header/header.component.ts
@@ -8,12 +8,44 @@ import { Component, HostListener } from '@angular/core';
 export class HeaderComponent {
   isNavOpen: boolean = false;
   offsetFlag: boolean = true;
+  isSearchOpen: boolean = false;
+
+  scrHeight: any;
+  scrWidth: any;
+  // Constructor
+  constructor() {
+    if ((this.scrWidth = window.screen.width >= 760)) {
+      this.isNavOpen = true;
+    } else {
+      this.isNavOpen = false;
+    }
+  }
 
   openNav(): void {
     this.isNavOpen = !this.isNavOpen;
   }
 
+  showSearch(): void {
+    this.isSearchOpen = !this.isSearchOpen;
+  }
+
+  // get the screen width and height
+  @HostListener('window:resize', ['$event'])
+  getScreenSize(event: Event) {
+    this.scrHeight = window.innerHeight;
+    this.scrWidth = window.innerWidth;
+
+    if (this.scrWidth >= 760) {
+      this.isNavOpen = true;
+    } else {
+      this.isNavOpen = false;
+    }
+  }
+
   @HostListener('window:scroll', ['$event']) getScrollHeight(event: any) {
-    if (window.scrollY > 0) this.isNavOpen = !this.isNavOpen;
+    if (window.scrollY > 0) {
+      this.isNavOpen = !this.isNavOpen;
+      this.isSearchOpen = !this.isSearchOpen;
+    }
   }
 }
