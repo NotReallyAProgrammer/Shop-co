6cbc9bcbd891006a01224a5b6477e953eefe65b6
NotReallyAProgrammer <73622276+NotReallyAProgrammer@users.noreply.github.com>
2024-01-18 17:51:49 +0800

    assets update

--- src/app/dummy-data/clothes-data.ts
+++ src/app/dummy-data/clothes-data.ts
@@ -1,7 +1,6 @@
 export const clothesInventory = [
   {
-    clotheImgUrl:
-      'https://s3-alpha-sig.figma.com/img/f04a/017d/b094f9a20c2328f54a31b153619784f3?Expires=1704672000&Signature=TFOjM9Jy5gZezOBxpiO9ugBP7YzORzPFD9o0~HVwx95SgI9XhUKXEZpCKIRKDKVKnQg~Y3GR-dPAvKnxsO91MV6kjvcHzTdb8tsYOCnbhZIHeIKtx6gNjBUzVFAF7mUEuGrL2Rxxs8JEOHY3~UXn4nZZuJjnwSzqgxy6snbBIpJvpDaw3kQGvNZFtVlkNe3ujR~GgghbnFQpFJvA4S-tv2yUTQdfS5hkQeb0gp6LlqBh1fqbJlhlCHhRaOJPsdoyddBIkfNV3R~RERxdih6lTafkMW4OzqILpMYVVAuZKDqA5JhoM0oqBRfmFExj6fYmrSwYjRJutUfMDOuBciCl8w__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4',
+    clotheImgUrl: '../../assets/clothes-img/Gradient-Tshirt.png',
     clotheName: 'Gradient Graphic T-Shirt',
     clotheRating: 3.5,
     clothePrice: 145,
@@ -41,8 +40,8 @@ export const clothesInventory = [
   },
 
   {
-    clotheImgUrl:
-      'https://s3-alpha-sig.figma.com/img/aecd/8196/485b30fd30b3226e09bb8f8e494c260b?Expires=1704672000&Signature=AO-AbjPbwLbiow-Dlp8K4dlc4Ome11fxhUlM9fN6UM7IsdMuY8gIJfv5Dts-nm-X73AgQOi~zpYpuK8~22tjiE85i-j6kx7L1plxFK6UAFYhDBsMjejLscrOsR4Gq0eCSJbZ8TAg4KIYWUKA-1Y73ZCEyWReMtb8UJZRYRhuZs3lp4xquRomBhiWPlz~968YhUUzIK3OO8isCM7IZ0uP2ckNCHgEs4S9qjIYfn48SPgKK5c4TJCLQfu7lYrB-oHXsk8T4grdrvisWPW7Eirdc1K37pumj08VPR3WKIBsp-kb6N7OtvUv0GQPREXVCB4XZgtYazxg0ncxRQsMR5~8fA__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4',
+    clotheImgUrl: '../../assets/clothes-img/Polo-with-tipping.png',
+
     clotheName: 'Polo with Tipping Details',
     clotheRating: 4.5,
     clothePrice: 180,
@@ -82,8 +81,8 @@ export const clothesInventory = [
   },
 
   {
-    clotheImgUrl:
-      'https://s3-alpha-sig.figma.com/img/6115/920b/12942762aefb7c7ac954e78b76284504?Expires=1704672000&Signature=W~LV3I5ARf-Kin~Zh-CdZucLTEjmGZinaeyDsqhUQxG90tKlrRLhHmiIE2Zio65szqHr57RGP1PAxtY7BYYyGyY0HTRyjwCVRfUsMnvNRZbdJPMkLs4jCconL94OFb~hZRhgj3ohtYHTTx~Ul-0WjzTkjnSkmCwAqRaklZd8W8-QrvB3xnrzfIC5m6Zl0RbsofEVwPlzhPI9rhX3km0rYEgqJXch~3Qx2pjdIKKaBAyEywA37ueDT58fbvoVswfqCQoBC40~Dwqhtv3I5vHDiX1VsZJjVBvLS1a0Wygsln7UwAUtiwTLtLIBfEpAywF7DdsHojgC1fZMnWrM~pkLHQ__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4 ',
+    clotheImgUrl: '../../assets/clothes-img/Black-Striped.png',
+
     clotheName: 'Black Striped T-shirt',
     clotheRating: 5,
     clothePrice: 120,
@@ -123,8 +122,8 @@ export const clothesInventory = [
   },
 
   {
-    clotheImgUrl:
-      'https://s3-alpha-sig.figma.com/img/769b/9d60/ff941dde9bc0e54431b8d8fe3182f5e9?Expires=1704672000&Signature=JbxwlJMVEs35xJZQugmHDux9LoE6rKLqPFUCJp65x0Cd0v4Campkk-vYzaLduaUDmAhoY5gAmVDAj-MOKpaOwQq57MyEM29vdi5gtxYWe66yjBi6cutn4qNGWfFfweaJBJL0g8Zvx~QtmCKtfMo8wGS5y0eY9lbqnQpmO5ISE-KuqKCJUwWXeuaCPjWbtCdzu6PGUVhFgt4a~wCJPf4I0RHF8zePL-9XDuc4e4Yjuzyvyk0abz9eMOc0NTYjZC-odK6lyhlj21PMf3-iuUjJ99EheV0rn2Qe5z7NED-iJck6JJC32nrzT3S-txIvybdOcH1HDVhn2nYmBH~pieDqxQ__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4',
+    clotheImgUrl: '../../assets/clothes-img/Skinny-fit.png',
+
     clotheName: 'Skinny Fit Jeans',
     clotheRating: 3.5,
     clothePrice: 240,
@@ -163,8 +162,8 @@ export const clothesInventory = [
     ],
   },
   {
-    clotheImgUrl:
-      'https://s3-alpha-sig.figma.com/img/bbf4/11c2/5fc84f87eeac1062fbe47f49c192d4f2?Expires=1704672000&Signature=RLo46WwBFah8z3f2TNISPSgMszsYqaf7GUNRaXDAV1CRAofKSqfOgmRFOf1qT3zTetVLRiZz97nuL84ISAH8fvvMg6XfKvKFgMCdB0giSk5athIccsPruPADYSpz~5hu5lu8P4IlghC7mN3f-ChaW1t29p3U2U69S3-Nwqq1Pf~NBUZMDuufk3ryrqU0Y~13yFD4viNs6HgZzzFWg5jaU4YFOMO79k~FtNfuV5IR7xqc0kgo-YILF-5OxqwZR~cxezk~SzVkdp8odqUW3tsRTiokEGMsBsS2fbVkAzCSuLTC0hIX6Akqqz-VZPGtUxKzugfKAtl5QFTlfeIfegf6mw__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4',
+    clotheImgUrl: '../../assets/clothes-img/Checkered-Shirt.png',
+
     clotheName: 'Checkered Shirt',
     clotheRating: 4.5,
     clothePrice: 180,
@@ -202,8 +201,8 @@ export const clothesInventory = [
     ],
   },
   {
-    clotheImgUrl:
-      'https://s3-alpha-sig.figma.com/img/3457/42c2/7cc557f42cf1d489f7cc811856b90e9f?Expires=1704672000&Signature=R4zWorJRE3~nwngVGsbzhtzrcqyyTYzDAc1cW42AwthaAbpC41BpcZaI6juDYY6MrKufJXs7dZLyr0Fi2ybUiOxac9O17fbc9GRP~V4cmeRRA3TN8KcUYu3WlETmU20lVCxXPnoDKwA9233DFdbqP51dz3drhkt0JUyvMMS~3N~ifl02u~lojPhsZFpF8fmPvvX~D1oytpA1Ik5bXKKq9qNFKD7V88rvJV8ubBnjCSf0ElfmRyhEdeuzCou~3~vg4l1udn0sqlqFQNsrsRGvG7m5-DdXp9y8kSyuSYXVSs3tgiuO8bmiLEz8Aft~NygiEC99OuewGOWybilGDFfRxw__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4',
+    clotheImgUrl: '../../assets/clothes-img/Sleeved-Striped.png',
+
     clotheName: 'Sleeved Striped T-Shirt',
     clotheRating: 4.5,
     clothePrice: 130,
@@ -242,8 +241,8 @@ export const clothesInventory = [
     ],
   },
   {
-    clotheImgUrl:
-      'https://s3-alpha-sig.figma.com/img/e01f/5d3c/d9029bd465a4c7158689ab1619693014?Expires=1704672000&Signature=q-WFEhX7rV3oxDTo5EQI4f~9g4cQywGoIY6kJZesCha7ftCiMLVXUB4JvnZVOG6tKLiJ2xk42fRCMNhqHb8R1OtX50-o0W3mqRnbBYrSvt2NHS44qTk3KR09udCK9rH01FhjBYDXUUq5jw6sV1Ar5eAigNiXyEoM7OpfhRla-XoZEJxUl0Oj3FjUjJGxFInpYTkA2sy6RlFE~2LXXQ7CHCDMyamC98-GpZHl4MUntOMzr0u4PJrBZBW987N7Q5YDniA25GbqwvIVES80VPZUNSwUvmtjLnmmCLuRhYGVzMLA093q92aQO2OxKAWcf836F7HELFjPkFfZUZbzmXIkMQ__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4',
+    clotheImgUrl: '../../assets/clothes-img/Vertical-Striped.png',
+
     clotheName: 'Vertical Striped Shirt',
     clotheRating: 5,
     clothePrice: 212,
@@ -283,8 +282,8 @@ export const clothesInventory = [
   },
 
   {
-    clotheImgUrl:
-      'https://s3-alpha-sig.figma.com/img/5723/4b01/d5fcac5632cf6823570ca2d1d53d7d73?Expires=1704672000&Signature=qkcZvBQaxYNFSoaEB6yV8tdIet7W8muvldDkGX35hFZez0T0J0dgeTvWxoy7cJoXYpeNjv8utvJsohShB2~p7ySSwCOJsaJbk1-ZXJIKbxCHvFKSPhr4BqeLt74nKdmyUl50raOZFCoip05laFFUOnHdXmKNq2xZ6pEd0yU8fx2OHqLhUHT0WxMJR4sK0i9PT-NaVsfRhdeVbT5m7iZLLjni~9RO8yvjTK6Q8plm5D~CYF~fBfCWLlhUPZMQlrfxBqgtJ0aAj71LIVceg94CbbzPmNT0aWNIOmc1lhxu9TiQ3ccJlrjrZwhBlSWi2pQbV~yNjSVaGleeUdluDrRtSg__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4',
+    clotheImgUrl: '../../assets/clothes-img/Courage-Graphic.png',
+
     clotheName: 'Courage Graphic T-Shirt',
     clotheRating: 4,
     clothePrice: 145,
@@ -324,8 +323,8 @@ export const clothesInventory = [
   },
 
   {
-    clotheImgUrl:
-      'https://s3-alpha-sig.figma.com/img/8951/5d71/4a66d9ca1401101dee4cc689f8bb5ad2?Expires=1704672000&Signature=pKuMxiyT9irWiEV1H5cV-B482XJ~cl4lNWi58QZYtPVhZeRG-ze58kFj4qyqKf26NCxw-woeQAtky45X0~w5oDGgzLSv17LiWS~L8usRognCaqutjCaa0RPQARF2bvLTNN2s2uVPAcZrm5E-un2bsixUyUh7uHEutxn2aZnx68kCPks2QelRYEKkJjbBZ2c4hcPFPTCej3jfldvjyzUH~q30LOyp7ocoN5oH-7HZSTevYiDhzNAThdx5olVY4TSaM-cR9wa1KB0qte5S~5FYPmSXvp3xocQcgLCbj1SSA6tWimOjLiMJYEvdwVrkhQYGj5hNxjw9p03ZumQCRKbz8Q__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4',
+    clotheImgUrl: '../../assets/clothes-img/Loose-Fit.png',
+
     clotheName: 'Loose Fit Bermuda',
     clotheRating: 3,
     clothePrice: 80,
@@ -366,9 +365,8 @@ export const clothesInventory = [
 
   // DUPLICATE DATA FOR PAGINATION
   {
-    clotheImgUrl:
-      'https://s3-alpha-sig.figma.com/img/f04a/017d/b094f9a20c2328f54a31b153619784f3?Expires=1704672000&Signature=TFOjM9Jy5gZezOBxpiO9ugBP7YzORzPFD9o0~HVwx95SgI9XhUKXEZpCKIRKDKVKnQg~Y3GR-dPAvKnxsO91MV6kjvcHzTdb8tsYOCnbhZIHeIKtx6gNjBUzVFAF7mUEuGrL2Rxxs8JEOHY3~UXn4nZZuJjnwSzqgxy6snbBIpJvpDaw3kQGvNZFtVlkNe3ujR~GgghbnFQpFJvA4S-tv2yUTQdfS5hkQeb0gp6LlqBh1fqbJlhlCHhRaOJPsdoyddBIkfNV3R~RERxdih6lTafkMW4OzqILpMYVVAuZKDqA5JhoM0oqBRfmFExj6fYmrSwYjRJutUfMDOuBciCl8w__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4',
-    clotheName: '2Gradient Graphic T-Shirt 2',
+    clotheImgUrl: '../../assets/clothes-img/Gradient-Tshirt.png',
+    clotheName: 'Gradient Graphic T-Shirt',
     clotheRating: 3.5,
     clothePrice: 145,
     isDiscounted: false,
@@ -407,9 +405,9 @@ export const clothesInventory = [
   },
 
   {
-    clotheImgUrl:
-      'https://s3-alpha-sig.figma.com/img/aecd/8196/485b30fd30b3226e09bb8f8e494c260b?Expires=1704672000&Signature=AO-AbjPbwLbiow-Dlp8K4dlc4Ome11fxhUlM9fN6UM7IsdMuY8gIJfv5Dts-nm-X73AgQOi~zpYpuK8~22tjiE85i-j6kx7L1plxFK6UAFYhDBsMjejLscrOsR4Gq0eCSJbZ8TAg4KIYWUKA-1Y73ZCEyWReMtb8UJZRYRhuZs3lp4xquRomBhiWPlz~968YhUUzIK3OO8isCM7IZ0uP2ckNCHgEs4S9qjIYfn48SPgKK5c4TJCLQfu7lYrB-oHXsk8T4grdrvisWPW7Eirdc1K37pumj08VPR3WKIBsp-kb6N7OtvUv0GQPREXVCB4XZgtYazxg0ncxRQsMR5~8fA__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4',
-    clotheName: '2Polo with Tipping Details',
+    clotheImgUrl: '../../assets/clothes-img/Polo-with-tipping.png',
+
+    clotheName: 'Polo with Tipping Details',
     clotheRating: 4.5,
     clothePrice: 180,
     isDiscounted: false,
@@ -448,9 +446,9 @@ export const clothesInventory = [
   },
 
   {
-    clotheImgUrl:
-      'https://s3-alpha-sig.figma.com/img/6115/920b/12942762aefb7c7ac954e78b76284504?Expires=1704672000&Signature=W~LV3I5ARf-Kin~Zh-CdZucLTEjmGZinaeyDsqhUQxG90tKlrRLhHmiIE2Zio65szqHr57RGP1PAxtY7BYYyGyY0HTRyjwCVRfUsMnvNRZbdJPMkLs4jCconL94OFb~hZRhgj3ohtYHTTx~Ul-0WjzTkjnSkmCwAqRaklZd8W8-QrvB3xnrzfIC5m6Zl0RbsofEVwPlzhPI9rhX3km0rYEgqJXch~3Qx2pjdIKKaBAyEywA37ueDT58fbvoVswfqCQoBC40~Dwqhtv3I5vHDiX1VsZJjVBvLS1a0Wygsln7UwAUtiwTLtLIBfEpAywF7DdsHojgC1fZMnWrM~pkLHQ__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4 ',
-    clotheName: '2Black Striped T-shirt',
+    clotheImgUrl: '../../assets/clothes-img/Black-Striped.png',
+
+    clotheName: 'Black Striped T-shirt',
     clotheRating: 5,
     clothePrice: 120,
     isDiscounted: true,
@@ -489,9 +487,9 @@ export const clothesInventory = [
   },
 
   {
-    clotheImgUrl:
-      'https://s3-alpha-sig.figma.com/img/769b/9d60/ff941dde9bc0e54431b8d8fe3182f5e9?Expires=1704672000&Signature=JbxwlJMVEs35xJZQugmHDux9LoE6rKLqPFUCJp65x0Cd0v4Campkk-vYzaLduaUDmAhoY5gAmVDAj-MOKpaOwQq57MyEM29vdi5gtxYWe66yjBi6cutn4qNGWfFfweaJBJL0g8Zvx~QtmCKtfMo8wGS5y0eY9lbqnQpmO5ISE-KuqKCJUwWXeuaCPjWbtCdzu6PGUVhFgt4a~wCJPf4I0RHF8zePL-9XDuc4e4Yjuzyvyk0abz9eMOc0NTYjZC-odK6lyhlj21PMf3-iuUjJ99EheV0rn2Qe5z7NED-iJck6JJC32nrzT3S-txIvybdOcH1HDVhn2nYmBH~pieDqxQ__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4',
-    clotheName: '2Skinny Fit Jeans',
+    clotheImgUrl: '../../assets/clothes-img/Skinny-fit.png',
+
+    clotheName: 'Skinny Fit Jeans',
     clotheRating: 3.5,
     clothePrice: 240,
     isDiscounted: true,
@@ -529,8 +527,8 @@ export const clothesInventory = [
     ],
   },
   {
-    clotheImgUrl:
-      'https://s3-alpha-sig.figma.com/img/bbf4/11c2/5fc84f87eeac1062fbe47f49c192d4f2?Expires=1704672000&Signature=RLo46WwBFah8z3f2TNISPSgMszsYqaf7GUNRaXDAV1CRAofKSqfOgmRFOf1qT3zTetVLRiZz97nuL84ISAH8fvvMg6XfKvKFgMCdB0giSk5athIccsPruPADYSpz~5hu5lu8P4IlghC7mN3f-ChaW1t29p3U2U69S3-Nwqq1Pf~NBUZMDuufk3ryrqU0Y~13yFD4viNs6HgZzzFWg5jaU4YFOMO79k~FtNfuV5IR7xqc0kgo-YILF-5OxqwZR~cxezk~SzVkdp8odqUW3tsRTiokEGMsBsS2fbVkAzCSuLTC0hIX6Akqqz-VZPGtUxKzugfKAtl5QFTlfeIfegf6mw__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4',
+    clotheImgUrl: '../../assets/clothes-img/Checkered-Shirt.png',
+
     clotheName: 'Checkered Shirt',
     clotheRating: 4.5,
     clothePrice: 180,
@@ -568,8 +566,8 @@ export const clothesInventory = [
     ],
   },
   {
-    clotheImgUrl:
-      'https://s3-alpha-sig.figma.com/img/3457/42c2/7cc557f42cf1d489f7cc811856b90e9f?Expires=1704672000&Signature=R4zWorJRE3~nwngVGsbzhtzrcqyyTYzDAc1cW42AwthaAbpC41BpcZaI6juDYY6MrKufJXs7dZLyr0Fi2ybUiOxac9O17fbc9GRP~V4cmeRRA3TN8KcUYu3WlETmU20lVCxXPnoDKwA9233DFdbqP51dz3drhkt0JUyvMMS~3N~ifl02u~lojPhsZFpF8fmPvvX~D1oytpA1Ik5bXKKq9qNFKD7V88rvJV8ubBnjCSf0ElfmRyhEdeuzCou~3~vg4l1udn0sqlqFQNsrsRGvG7m5-DdXp9y8kSyuSYXVSs3tgiuO8bmiLEz8Aft~NygiEC99OuewGOWybilGDFfRxw__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4',
+    clotheImgUrl: '../../assets/clothes-img/Sleeved-Striped.png',
+
     clotheName: 'Sleeved Striped T-Shirt',
     clotheRating: 4.5,
     clothePrice: 130,
@@ -608,8 +606,8 @@ export const clothesInventory = [
     ],
   },
   {
-    clotheImgUrl:
-      'https://s3-alpha-sig.figma.com/img/e01f/5d3c/d9029bd465a4c7158689ab1619693014?Expires=1704672000&Signature=q-WFEhX7rV3oxDTo5EQI4f~9g4cQywGoIY6kJZesCha7ftCiMLVXUB4JvnZVOG6tKLiJ2xk42fRCMNhqHb8R1OtX50-o0W3mqRnbBYrSvt2NHS44qTk3KR09udCK9rH01FhjBYDXUUq5jw6sV1Ar5eAigNiXyEoM7OpfhRla-XoZEJxUl0Oj3FjUjJGxFInpYTkA2sy6RlFE~2LXXQ7CHCDMyamC98-GpZHl4MUntOMzr0u4PJrBZBW987N7Q5YDniA25GbqwvIVES80VPZUNSwUvmtjLnmmCLuRhYGVzMLA093q92aQO2OxKAWcf836F7HELFjPkFfZUZbzmXIkMQ__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4',
+    clotheImgUrl: '../../assets/clothes-img/Vertical-Striped.png',
+
     clotheName: 'Vertical Striped Shirt',
     clotheRating: 5,
     clothePrice: 212,
@@ -649,8 +647,8 @@ export const clothesInventory = [
   },
 
   {
-    clotheImgUrl:
-      'https://s3-alpha-sig.figma.com/img/5723/4b01/d5fcac5632cf6823570ca2d1d53d7d73?Expires=1704672000&Signature=qkcZvBQaxYNFSoaEB6yV8tdIet7W8muvldDkGX35hFZez0T0J0dgeTvWxoy7cJoXYpeNjv8utvJsohShB2~p7ySSwCOJsaJbk1-ZXJIKbxCHvFKSPhr4BqeLt74nKdmyUl50raOZFCoip05laFFUOnHdXmKNq2xZ6pEd0yU8fx2OHqLhUHT0WxMJR4sK0i9PT-NaVsfRhdeVbT5m7iZLLjni~9RO8yvjTK6Q8plm5D~CYF~fBfCWLlhUPZMQlrfxBqgtJ0aAj71LIVceg94CbbzPmNT0aWNIOmc1lhxu9TiQ3ccJlrjrZwhBlSWi2pQbV~yNjSVaGleeUdluDrRtSg__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4',
+    clotheImgUrl: '../../assets/clothes-img/Courage-Graphic.png',
+
     clotheName: 'Courage Graphic T-Shirt',
     clotheRating: 4,
     clothePrice: 145,
@@ -690,8 +688,8 @@ export const clothesInventory = [
   },
 
   {
-    clotheImgUrl:
-      'https://s3-alpha-sig.figma.com/img/8951/5d71/4a66d9ca1401101dee4cc689f8bb5ad2?Expires=1704672000&Signature=pKuMxiyT9irWiEV1H5cV-B482XJ~cl4lNWi58QZYtPVhZeRG-ze58kFj4qyqKf26NCxw-woeQAtky45X0~w5oDGgzLSv17LiWS~L8usRognCaqutjCaa0RPQARF2bvLTNN2s2uVPAcZrm5E-un2bsixUyUh7uHEutxn2aZnx68kCPks2QelRYEKkJjbBZ2c4hcPFPTCej3jfldvjyzUH~q30LOyp7ocoN5oH-7HZSTevYiDhzNAThdx5olVY4TSaM-cR9wa1KB0qte5S~5FYPmSXvp3xocQcgLCbj1SSA6tWimOjLiMJYEvdwVrkhQYGj5hNxjw9p03ZumQCRKbz8Q__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4',
+    clotheImgUrl: '../../assets/clothes-img/Loose-Fit.png',
+
     clotheName: 'Loose Fit Bermuda',
     clotheRating: 3,
     clothePrice: 80,
